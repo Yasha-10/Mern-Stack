377a46f6dd28f806ff8ae2d57be09a8420c40250
Yasha-10 <yasha.gehlot2gmail.com>
2021-05-31 14:34:44 +0530

    Assignment upload

---
+++ Assingment 6/index.js
@@ -0,0 +1,10 @@
+//console.log("External File");
+//document.getElementById("heading").innerHTML = "I am Changed"
+
+var a = "Hello";
+var b = "20";
+var c = "10.5"
+var d = ["hello", "world"];
+var e = ["hello", 1];
+
+console.log(typeof a, b, c, d, e);
