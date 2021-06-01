3fb813795df56bda4029ce5225437eebfd815a79
Yasha-10 <yasha.gehlot2gmail.com>
2021-06-01 08:23:52 +0530

    upload

--- Assingment 6/index.js
+++ Assingment 6/index.js
@@ -1,10 +1,19 @@
-//console.log("External File");
-//document.getElementById("heading").innerHTML = "I am Changed"
+//To show an alert message to the user
+alert(2 + 3)
+console.log(2 + 3)
 
-var a = "Hello";
-var b = "20";
-var c = "10.5"
-var d = ["hello", "world"];
-var e = ["hello", 1];
+//to print on web page
+//window.print()
 
-console.log(typeof a, b, c, d, e);
+
+var x = 10;
+var y = 20;
+z = 30;
+z = 40;
+//console.log(x + y + z);
+
+
+var person = "Yasha";
+var person = "   Yasha";
+var pi = 3.14;
+var area = 100;
